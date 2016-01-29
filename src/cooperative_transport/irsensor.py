89686b72d166d47eb99df0b50059243e65ff63f3
Nicola Piga <nicolapiga@gmail.com>
2016-01-29 05:33:21 +0000

    Refactoring of irsensor.py.

--- src/cooperative_transport/irsensor.py
+++ src/cooperative_transport/irsensor.py
@@ -1,161 +1,202 @@
 from sympy.mpmath import *
 import sys
 
-
 class IrSensor():
-    """Simulate an IR sensor behevior using Hoyuko laser data.
+    """Simulate the irobot create 2 ir light bumper sensor using laser range finder data.
 
+    The sensor modelling is taken from the USA patent US2014/0188325.
+    
     Attributes:
-    laser_resolution (float): laser resolution in radiants [2 * pi / (laser ray number).
-    epsilon_max (float): laser ray maximum relative angle compatible with the IR sensor geometry.
-    roomba_radius (float): roomba radius in meters.
-    relative_indices(int[]): list of relative indices used to indicate a compatible laser ray.
-    bounds (dictionary): dictionary with minimum and maximum value accepted for every laser ray.
+    alpha_o (float): ir emitter/detector outer spread off-center angle (rad)
+    alpha_i (float): ir emitter/detector inner spread off-center angle (rad)
+    beta (float): angle between emission and detection fields (rad)
+    housing_length (float): emitter and detector housing length (meters)    
+    laser_resolution (float): laser angular resolution (rad)
+    epsilon_max (float): maximum positive angle of a laser ray intersecting the ir detection area (rad)
+    robot_radius (float): irobot create radius (meters)
+    bounds (dictionary): minimum and maximum lengths describing the ir detection area (meters)
+    sweep_angles_indexes(int[]): list of indexes j such that a laser ray with angle 
+                                 j*laser_resolution intersects the ir detection area
+
+    Public methods:
+    laser_to_ir(data, angle_0, ir_angle) : convert laser range finder data to ir sensor reading
     """
-    def __init__(self, alpha_o, alpha_i, beta, sensor_length, roomba_radius, laser_resolution):
+
+    def __init__(self, alpha_o, alpha_i, beta, housing_length, laser_resolution, robot_radius):
+        """Initialize the object.
+
+        Arguments:
+        alpha_o (float): ir emitter/detector outer spread off-center angle (rad)
+        alpha_i (float): ir emitter/detector inner spread off-center angle (rad)
+        beta (float): angle between emission and detection fields (rad)
+        housing_length (float): emitter and detector housing length (meters)    
+        laser_resolution (float): laser angular resolution (rad)
+        robot_radius (float): irobot create radius (meters)
         """
-        Args:
-        alpha_o (float): 'external' angle, in radiants, used to describe IR sensor geometry.
-        alpha_i (float): 'internal' angle, in radiants, used to describe IR sensor geometry.
-        beta (float): angle, in radiants, used to describe IR sensor geometry.
-        sensor_length (float): geometry length of IR sensor.
-        roomba_radius (float): roomba radius in meters.
-        laser_resolution (float): laser resolution in radiants [2 * pi / (laser ray number).
-        """
+        self.alpha_o = alpha_o
+        self.alpha_i = alpha_i
+        self.beta = beta
+        self.housing_length = housing_length
         self.laser_resolution = laser_resolution
-        self.roomba_radius = roomba_radius
-        self.epsilon_max = self.epsilon_max_evaluation(alpha_o, alpha_i, beta, sensor_length, self.roomba_radius)
+        self.robot_radius = robot_radius
+        self._epsilon_max = 0
+        self.eval_epsilon_max()
+        self.sweep_angles_indexes = []
+        self.eval_sweep_angles_indexes()
+        self.bounds = {}
+        self.eval_bounds()
+
         
-        acceptable_laser_number = int(round(self.epsilon_max / self.laser_resolution))
+    @property
+    def epsilon_max(self):
+        """Return epsilon_max."""
+        return self._epsilon_max
+
+    def get_bounds(self):
+        """Return self.bounds."""
+        return self.bounds
+
+    def eval_epsilon_max(self):
+        """Evaluate the maximum positive angle of a laser ray intersecting the ir detection area (rad)."""
+        self._epsilon_max = float(asin((self.housing_length * csc(self.alpha_i - self.alpha_o + self.beta) * sin(self.alpha_i + self.alpha_o))/\
+                    (sqrt((2 * self.robot_radius + self.housing_length * cot(self.alpha_i + self.beta/2))**2 +\
+                    2 * self.housing_length * cot(self.alpha_i + self.beta/2) * (2 * self.robot_radius + self.housing_length *\
+                    cot(self.alpha_i + self.beta/2)) * csc(self.alpha_i - self.alpha_o + self.beta) * sin(self.alpha_i + self.alpha_o) +\
+                    self.housing_length**2 * (csc(self.alpha_i + self.beta/2))**2 * (csc(self.alpha_i - self.alpha_o + self.beta))**2 *\
+                    (sin(self.alpha_i+self.alpha_o))**2))))
+
+    def eval_sweep_angles_indexes(self):
+        """Evaluate the indexes relative to the laser ray sweep angles."""
+        # how many laser rays inside the ir detection area?
+        number = int(round(self._epsilon_max / self.laser_resolution))
    
-        self.relative_indices = [index for index in range(-acceptable_laser_number, acceptable_laser_number + 1)]
+        # discrete indexes relative to the angles of the laser rays intersecting
+        # the ir detection area
+        self.sweep_angles_indexes = [index for index in range(-number, number + 1)]
 
-        self.bounds = {index: [self.generate_lower_bound(alpha_i, beta, sensor_length, self.roomba_radius, index, laser_resolution),\
-                    self.generate_upper_bound(alpha_o, beta, sensor_length, self.roomba_radius, index, laser_resolution)]\
-                    for index in self.relative_indices}
+    def eval_lower_bound(self, index):
+        """Evaluate the lower bound given the index of the laser ray sweep angle.
 
-    def epsilon_max_evaluation(self, alpha_o, alpha_i, beta, sensor_length, roomba_radius):
-        """
-        Return epsilon max value, in radiants. 
         Args:
-        alpha_o (float): 'external' angle, in radiants, used to describe IR sensor geometry.
-        alpha_i (float): 'internal' angle, in radiants, used to describe IR sensor geometry.
-        beta (float): angle, in radiants, used to describe IR sensor geometry.
-        sensor_length (float): geometry length of IR sensor.
-        roomba_radius (float): roomba radius in meters.
-        """
-        epsilon_max = asin((sensor_length * csc(alpha_i - alpha_o + beta) * sin(alpha_i + alpha_o))/\
-                    (sqrt((2 * roomba_radius + sensor_length * cot(alpha_i + beta/2))**2 +\
-                    2 * sensor_length * cot(alpha_i + beta/2) * (2 * roomba_radius + sensor_length *\
-                    cot(alpha_i + beta/2)) * csc(alpha_i - alpha_o + beta) * sin(alpha_i + alpha_o) +\
-                    sensor_length**2 * (csc(alpha_i + beta/2))**2 * (csc(alpha_i - alpha_o + beta))**2 *\
-                    (sin(alpha_i+alpha_o))**2)))
-
-        return float(epsilon_max)
-
-
-    def generate_lower_bound(self, alpha_i, beta, sensor_length, roomba_radius, index, laser_resolution):
-        """
-        Return lower bound for the laser ray specified by index.
-        Args:
-        alpha_i (float): 'internal' angle, in radiants, used to describe IR sensor geometry.
-        beta (float): angle, in radiants, used to describe IR sensor geometry.
-        sensor_length (float): geometry length of IR sensor.
-        roomba_radius (float): roomba radius in meters.
-        index (int): relative index of laser whereof lower bound is evaluated.
-        laser_resolution (float): laser resolution in radiants [2 * pi / (laser ray number)].
+        index (int): laseer ray sweep angle index
         """        
-        angle = index * laser_resolution
-        lower_bound = 1./2 * csc(alpha_i + beta/2 - abs(angle)) * (sensor_length * cos(alpha_i + beta/2) +\
-                    2 * roomba_radius * sin(alpha_i + beta/2))
+        angle = index * self.laser_resolution
+        lower_bound = 1./2 * csc(self.alpha_i + self.beta/2 - abs(angle)) * (self.housing_length * cos(self.alpha_i + self.beta/2) +\
+                    2 * self.robot_radius * sin(self.alpha_i + self.beta/2))
 
         return float(lower_bound)
 
+    def eval_upper_bound(self, index):
+        """Return the upper bound given the index of the laser ray sweep angle.
 
-    def generate_upper_bound(self, alpha_o, beta, sensor_length, roomba_radius, index, laser_resolution):
-        """
-        Return upper bound for the laser ray specified by index.
         Args:
-        alpha_o (float): 'external' angle, in radiants, used to describe IR sensor geometry.
-        beta (float): angle, in radiants, used to describe IR sensor geometry.
-        sensor_length (float): geometry length of IR sensor.
-        roomba_radius (float): roomba radius in meters.
-        index (int): relative index of laser whereof lower bound is evaluated.
-        laser_resolution (float): laser resolution in radiants [2 * pi / (laser ray number).
-        """
+        index (int): laser sweep angle index
+        """        
         angle = index * laser_resolution
-        upper_bound = 1./2 * sqrt(((sec(angle))**2 * (sensor_length - 2 * roomba_radius * tan(alpha_o -\
-                    beta/2))**2)/(tan(1./2 * (-2 * alpha_o + beta)) + tan(abs(angle)))**2)
+        upper_bound = 1./2 * sqrt(((sec(angle))**2 * (self.housing_length - 2 * self.robot_radius * tan(self.alpha_o -\
+                    self.beta/2))**2)/(tan(1./2 * (-2 * self.alpha_o + self.beta)) + tan(abs(angle)))**2)
 
         return float(upper_bound)
 
-    def get_bounds(self):
-        return self.bounds
+    def eval_bounds(self):
+        """Evaluate the bounds that describe the ir detection area."""        
+        self.bounds = {index: [self.eval_lower_bound(index), self.eval_upper_bound(index)]\
+                       for index in self.sweep_angles_indexes}
 
-    def get_epsilon_max(self):
-        return self.epsilon_max/float(pi/180)
-
-    def distance_from_sensor(self, data, index):
-        """
-        Return distance from the center of IR sensor.
+    def distance_from_housing(self, distance_from_laser, index):
+        """Return the distance from the center of the emitter/detector housing.
+        
         Args:
-        data (float): distance from the center of Hokuyo laser.
-        index (int): laser ray relative index.
+        distance_from_laser (float): distance from the center of laser range finder
+        index (int): sweep angle index
         """
         angle = self.laser_resolution * index
-        return float(data * cos(angle) - self.roomba_radius)
+        return float(distance_from_laser * cos(angle) - self.robot_radius)
 
-    def convert_from_data_laser(self, data, angle_0, ir_angle):
-        """
-        Convert laser data into IR sensor data (range from 4950 to 0).
+    def laser_to_ir(self, data, angle_0, ir_angle):
+        """Convert laser range finder data into IR sensor reading.
+
+        Return an ir sensor reading between 0 and 4095.
+
         Args:
-        data (float[]): Hokuyo laser data.
-        angle_0 (int): angle, in radiants, corresponding to the first position in data list.
-        ir_angle (float): angular position of IR sensor in roomba robot.
+        data (float[]): laser range finder data
+        angle_0 (int): angle corresponding to the first item of data (rad)
+        ir_angle (float): angular position of the emitter/detector housing (-180, 180)
         """
 
-        max_distance = 0
-        min_distance = 4095
+        # ir light bumper sensor values range as reported in the
+        # irobot create 2 open interface
+        irobot_min_ir = 0
+        irobot_max_ir = 4095
 
-        #Keep angle from 0 to 2 * pi   
-        angle = ir_angle
-        if(angle < 0):
-            angle = float(angle + 2 * pi)
+        # force ir_angle inside [0, 2pi]
+        ir_angle = float(pi) / 180 * ir_angle
+        if(ir_angle < 0):
+            ir_angle = float(ir_angle + 2 * pi)
     
-        angle_index = int(round((angle - angle_0) / self.laser_resolution))
+        # index relative to the angle ir_angle
+        ir_angle_index = int(round((ir_angle - angle_0) / self.laser_resolution))
 
-        if data[angle_index] < self.bounds[0][0]:
-            print(min_distance)
-            return min_distance
-
-        acceptable_data = {relative_index: data[angle_index + relative_index]\
-                        for relative_index in self.relative_indices\
-                        if (data[angle_index + relative_index] < self.bounds[relative_index][1]\
-                        and data[angle_index + relative_index] > self.bounds[relative_index][0])}
+        # laser range relative to the laser ray with angle = ir_angle
+        range_at_ir_angle = data[ir_angle_index + 0]
         
-        if not bool(acceptable_data):
-            print(max_distance)
-            return max_distance
+        # if range_at_ir_angle is less than the lower bound relative to 
+        # the angle = ir_angle  then the object cannot be identified by the ir detector
+        if range_at_ir_angle < self.bounds[0][0]:
+            return irobot_min_ir
 
-        object_distance = min([self.distance_from_sensor(acceptable_data[relative_index], relative_index) for relative_index in acceptable_data])
-                 
-        converted_object_distance = int(round((1 - (object_distance - self.bounds[0][0])/ (self.bounds[0][1] - self.bounds[0][0])) * min_distance))
+        # find all the admissible ranges, i.e, inside [lower_bound, upper_bound],
+        # and evaluate them wrt to the center of the emitter/detector housing
+        admissible_ranges = [self.distance_from_housing(data[ir_angle_index + index], index)\
+                             for index in self.sweep_angles_indexes\
+                             if (data[ir_angle_index + index] < self.bounds[index][1]\
+                            and data[ir_angle_index + index] > self.bounds[index][0])]
         
-        return converted_object_distance
+        # if no adimssible ranges the object cannot be identified by the ir detector
+        if not admissible_ranges:
+            return irobot_min_ir
+
+        # take the minimum 
+        equivalent_range = min(admissible_ranges)
+
+        # scale the value so that it adhere with the irobot create 2 open specification (0-4095)
+        max_distance = self.bounds[0][1] - self.robot_radius
+        ir_value = int(round((1 - equivalent_range/max_distance)*irobot_max_ir))
+        
+        return ir_value
 
 if __name__ == "__main__":
+    """ Some demos."""
 
+    # sensor specs
     k = float(pi/180)
     alpha_o = 10 * k
     alpha_i = 25 * k
     beta = 50 * k
-    sensor_length = 0.034
-    roomba_radius = 0.1696
-    laser_resolution = 360*k/360
-    data = [0.2 for i in range(360)]
-    ir = IrSensor(alpha_o, alpha_i, beta, sensor_length, roomba_radius, laser_resolution)
-    object_distance = ir.convert_from_data_laser(data, 0, -3.14)
-   
-    print('epsilon_max', ir.get_epsilon_max())
+    housing_length = 0.034
+
+    # laser specs
+    angle_0 = float(pi)
+    data_length = 360
+    laser_resolution = data_length*k/360
+
+    # robot specs
+    robot_radius = 0.1696
+    max_radius = robot_radius + 0.065
+
+    # istance of IrSensor
+    ir = IrSensor(alpha_o, alpha_i, beta, housing_length, laser_resolution, robot_radius)
+    ir_angle = 0
+
+    # some outputs
+    min_radius = ir.get_bounds()[0][0]
+    number_steps = 50
+    step = (max_radius - min_radius) / number_steps
+
+    print('epsilon_max', ir.epsilon_max * 180 / float(pi))
     print('bounds', ir.get_bounds())
-    print('object_distance', object_distance)
+    for i in range(0, number_steps):
+        radius = min_radius + i*step
+        fake_data = [radius for  j in range (data_length)]
+        ir_reading = ir.laser_to_ir(fake_data, ir_angle, angle_0)
+        print "constant radius: %f, ir: %f" % (radius, ir_reading)
