3b5beda2a0d481fc28d187768220edd4872db84d
Giulio Romualdi <giulio.romu@gmail.com>
2016-02-21 15:42:28 +0100

    Added saturation to the consensus controller ouput.

--- src/cooperative_transport/consensus.py
+++ src/cooperative_transport/consensus.py
@@ -1,11 +1,15 @@
-def consensus(state, neighbours_state, reference):
+from numpy import sign
+
+def consensus(state, neighbours_state, reference, max_control):
     """Return consensus controller output for a SISO system.
     
     Arguments:
     state (float): robot angular state
     neighbors_state (float[]): neighbors angular state
     reference (float): desired angular state
+    max_control (float): maximum allowed output
     """
     neighbours_state.append(reference)
     output = sum([neighbour_state - state for neighbour_state in neighbours_state])
-    return output
+    saturated_output = sign(output) * min(abs(output), max_control)
+    return saturated_output
