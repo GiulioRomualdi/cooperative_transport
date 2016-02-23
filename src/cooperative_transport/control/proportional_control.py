116b8aa9e0e6437e6d1307cc81545f0600b13e80
Giulio Romualdi <giulio.romu@gmail.com>
2016-02-23 15:30:47 +0100

    Implement a basic P (proportional) controller

---
+++ src/cooperative_transport/control/proportional_control.py
@@ -0,0 +1,14 @@
+from ..utils import saturation
+
+def proportional_control(k_p, r, y, u_max):
+    """Implement proportional control law.
+
+    Arguments:
+    k_p (float): Proportional gain
+    r (float): reference signal
+    y (float): system output signal
+    u_max (float): maximum control effort
+    """
+    u = k_p * (r - y)
+    saturated_u = saturation(u ,u_max)
+    return saturated_u
