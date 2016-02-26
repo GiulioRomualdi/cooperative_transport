be36c4c8d5c6255f01d89b894fe51a277e6d8838
Nicola Piga <nicolapiga@gmail.com>
2016-02-26 14:58:38 +0000

    Implement the class Segment required for issue #5.

--- src/cooperative_transport/utils.py
+++ src/cooperative_transport/utils.py
@@ -34,3 +34,59 @@ def saturation(input_value, max_value):
     """
     saturated_value = np.sign(input_value) * min(abs(input_value), max_value)
     return saturated_value
+
+class Segment:
+    """Segment between two points p0 and p1.
+
+    p(t) = (1 - t) * p0 + t * p1 t in [0,1]
+    """
+
+    def __init__(self, p0, p1):
+        self._p0 = np.array(p0)
+        self._p1 = np.array(p1)
+
+    @property
+    def p0(self):
+        return self._p0.tolist()
+        
+    @p0.setter
+    def p0(self, p0):
+        self._p0 = np.array(p0)
+
+    @property
+    def p1(self):
+        return self._p1.tolist()
+
+    @p1.setter
+    def p1(self, p1):
+        self._p1 = np.array(p1)
+
+    def normal(self):
+        """Return the normal direction pointing inward"""
+        difference = self._p1 - self._p0
+        length = np.linalg.norm(difference)
+        normal = np.array([float(-difference[1]), float(difference[0])])/length
+
+        return normal.tolist()
+
+    def point(self, t):
+        """Return the point p(t) = (1 - t) * p0 + t * p1.
+
+        Arguments:
+        t (float): parameter t in [0,1]
+        """
+        
+        p = (1 - t) * self._p0 + t * self._p1
+
+        return p.tolist()
+
+    def length(self):
+        """Return the length of the segment."""
+        difference = self._p1 - self._p0
+        return float(np.linalg.norm(difference))
+
+    def __ge__(self, other):
+        if isinstance(other, Segment):
+            return self.length() >= other.length()
+        else:
+            return NotImplemented        
