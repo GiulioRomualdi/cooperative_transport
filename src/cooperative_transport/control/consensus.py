0e8cf2f2c7b3cf928ab26acce4e0d5260595a4eb
Nicola Piga <nicolapiga@gmail.com>
2016-03-03 21:06:32 +0000

    Implement overturning avoidance in consensus controller

--- src/cooperative_transport/control/consensus.py
+++ src/cooperative_transport/control/consensus.py
@@ -1,4 +1,11 @@
 from cooperative_transport.utils import saturation 
+import numpy as np
+
+def correct_delta(error):
+    if abs(error) > np.pi:
+        error += -2 * np.pi * np.sign(error)
+
+    return error
 
 def consensus(state, neighbours_state, reference, max_control):
     """Return consensus controller output for a SISO system.
@@ -10,6 +17,6 @@ def consensus(state, neighbours_state, reference, max_control):
         max_control (float): maximum allowed output
     """
     neighbours_state.append(reference)
-    output = sum([neighbour_state - state for neighbour_state in neighbours_state])
+    output = sum([correct_delta(neighbour_state - state) for neighbour_state in neighbours_state])
     saturated_output = saturation(output, max_control)
     return saturated_output
